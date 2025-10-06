915e9a0e368d85b0039268d39fef2203122a2dc1
VietnameseRick <95068784+VietnameseRick@users.noreply.github.com>
2025-10-07 00:44:53 +0700

    Convert packages/site to submodule (fhevm-poker-nextjs)

--- packages/site/abi/FHEPokerAddresses.ts
+++
@@ -1,9 +0,0 @@
-
-/*
-  This file is auto-generated.
-  By commands: 'npx hardhat deploy' or 'npx hardhat node'
-*/
-export const FHEPokerAddresses = { 
-  "11155111": { address: "0xe664bB7139cdA7DfD188E5C2289ecaF6fb677A5E", chainId: 11155111, chainName: "sepolia" },
-  "31337": { address: "0x5FbDB2315678afecb367f032d93F642f64180aa3", chainId: 31337, chainName: "hardhat" },
-};
